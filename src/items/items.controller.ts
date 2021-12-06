a2e5a005c0cf2afe12a4467deb508c875fb964d3
shahar <shaharm@gmail.com>
2021-12-06 21:07:43 +0200

    [MINOR] comments & naming

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Put, Query, Res, StreamableFile  } from '@nestjs/common';
+import { Controller, Get, Query, StreamableFile  } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './interfaces/item.interface';
 import { createReadStream } from 'fs';
@@ -9,10 +9,10 @@ import { join } from 'path';
 export class ItemsController {
 
   constructor(private readonly itemsService: ItemsService) { }
-
+  
   @Get()
-  findAll(@Query('startDate') startDate: number, @Query('endDate') endDate: number): Promise<Item[]> {
-    return this.itemsService.findAll(startDate, endDate);
+  find(@Query('startDate') startDate: number, @Query('endDate') endDate: number): Promise<Item[]> {
+    return this.itemsService.find(startDate, endDate);
   }
 
   @Get('/file')
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './interfaces/item.interface';
 import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
 import fs = require('fs');
+import { InjectModel } from '@nestjs/mongoose';
 const { Parser } = require('json2csv');
 const fields = ['createdAt', 'customerId', 'invoiceId'];
 
@@ -12,12 +12,20 @@ export class ItemsService {
 
   constructor(@InjectModel('items') private readonly itemModel: Model<Item>) { }
 
-  async findAll(startDate: number, endDate: number): Promise<Item[]> {
+  /**
+   * @param  {number} startDate - The start of the date to filter.
+   * @param  {number} endDate - The end of the date to filter.
+   * @returns Promise
+   */
+  async find(startDate: number, endDate: number): Promise<Item[]> {
     let items: Item[] = await this.itemModel.find({ $and: [{ createdAt: { $gte: startDate } }, { createdAt: { $lte: endDate } }] });
     this.saveToFile(items);
     return items
   }
-
+  /**
+   * @param  {Item[]} items - The items which needs to be saved to the file.
+   * @returns void
+   */
   private saveToFile(items: Item[]): void {
     const json2csvParser = new Parser({ fields });
     const csv = json2csvParser.parse(items);
